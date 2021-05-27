0fc090089200fb41977508bc5cf4b7da599fdf64
S-H JIN <jinsuhyeon00@naver.com>
2021-05-27 15:09:35 +0900

    Updated lotto ball images

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -14,46 +14,64 @@ fun getRandomLottoNumber () : Int{
 }
 
 
-fun getRandomLottoNumbers(): MutableList<Int>{
+fun getRandomLottoNumbers(): MutableList<Int> {
     val lottoNumbers = mutableListOf<Int>()
+/*
+for (i in 1..6) {
+    var number = 0
+    do {
+        number = getRandomLottoNumber()
+    } while (lottoNumbers.contains(number))
+    lottoNumbers.add(number)
+}
+return lottoNumbers
+}
+*/
 
-    while(true){
-        var numbers: Int = getRandomLottoNumber()
-        var flag_existing: Int = 0
-        if(lottoNumbers.contains(numbers)){
-            flag_existing =1
-            break;
-        }
-        if(flag_existing.equals(1))
-            continue
-        else
-            lottoNumbers.add(numbers)
-        if(lottoNumbers.size>=0)
-            break;
+while(true){
+    val number = getRandomLottoNumber()
+    var flag_exist = 0
+
+    if(lottoNumbers.size < 1){
+        lottoNumbers.add(number)
+        continue
     }
-    return lottoNumbers
+    else {
+        for (j in 0 until lottoNumbers.size) {
+            if (number == lottoNumbers[j]) {
+                flag_exist = 1
+                break
+            }
+        }
+        if (flag_exist == 0)
+            lottoNumbers.add(number)
+        if (lottoNumbers.size >= 6)
+            break
+    }
+}
+return lottoNumbers
 }
 
-    class MainActivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-            Toast.makeText(this, "Main Activity", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Main Activity", Toast.LENGTH_LONG).show()
 
-            val cardRandom = findViewById<CardView>(R.id.cardRandom)
-            cardRandom.setOnClickListener {
-                val intent = Intent(this, ResultActivity::class.java)
-                intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers ()))
-                startActivity(Intent(this, ResultActivity::class.java))
-
-            }
-            findViewById<View>(R.id.cardConstell).setOnClickListener {
-                startActivity(Intent(this, ConstellationActivity::class.java))
-            }
-            findViewById<View>(R.id.cardName).setOnClickListener {
-                startActivity(Intent(this, NameActivity::class.java))
-            }
+        val cardRandom = findViewById<CardView>(R.id.cardRandom)
+        cardRandom.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+            startActivity(Intent(this, ResultActivity::class.java))
 
         }
+        findViewById<View>(R.id.cardConstell).setOnClickListener {
+            startActivity(Intent(this, ConstellationActivity::class.java))
+        }
+        findViewById<View>(R.id.cardName).setOnClickListener {
+            startActivity(Intent(this, NameActivity::class.java))
+        }
+
     }
+}
--- app/src/main/java/com/example/lotto/ResultActivity.kt
+++ app/src/main/java/com/example/lotto/ResultActivity.kt
@@ -2,13 +2,55 @@ package com.example.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 
 class ResultActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
         Toast.makeText(this,"Result Activity", Toast.LENGTH_LONG).show()
+
+
+        val result = intent.getIntegerArrayListExtra("result")
+
+        val result_sorted = result?.sorted()
+
+        val lottoBallImageStartId = R.drawable.ball_01
+/*        val lottoBallImageId2 = R.drawable.ball_02 // 150
+        val lottoBallImageId3 = R.drawable.ball_03 // 148*/
+        val imageView1 = findViewById<ImageView>(R.id.imageView1)
+        val imageView2 = findViewById<ImageView>(R.id.imageView2)
+        val imageView3 = findViewById<ImageView>(R.id.imageView3)
+        val imageView4 = findViewById<ImageView>(R.id.imageView4)
+        val imageView5 = findViewById<ImageView>(R.id.imageView5)
+        val imageView6 = findViewById<ImageView>(R.id.imageView6)
+
+        imageView1.setImageResource(lottoBallImageStartId + result_sorted!![0] - 1)
+        imageView2.setImageResource(lottoBallImageStartId + result_sorted[1] - 1)
+        imageView3.setImageResource(lottoBallImageStartId + result_sorted[2] - 1)
+        imageView4.setImageResource(lottoBallImageStartId + result_sorted[3] - 1)
+        imageView5.setImageResource(lottoBallImageStartId + result_sorted[4] - 1)
+        imageView6.setImageResource(lottoBallImageStartId + result_sorted[5] - 1)
+
+    }
+    private fun updateLottoBallImages(result_sorted : List<Int>) {
+        val lottoImageStartId = R.drawable.ball_01
+
+        val imageView1 = findViewById<ImageView>(R.id.imageView1)
+        val imageView2 = findViewById<ImageView>(R.id.imageView2)
+        val imageView3 = findViewById<ImageView>(R.id.imageView3)
+        val imageView4 = findViewById<ImageView>(R.id.imageView4)
+        val imageView5 = findViewById<ImageView>(R.id.imageView5)
+        val imageView6 = findViewById<ImageView>(R.id.imageView6)
+
+        imageView1.setImageResource(lottoImageStartId + result_sorted?.get(0)!! - 1)
+        imageView2.setImageResource(lottoImageStartId + result_sorted?.get(1) - 1)
+        imageView3.setImageResource(lottoImageStartId + result_sorted?.get(2) - 1)
+        imageView4.setImageResource(lottoImageStartId + result_sorted?.get(3) - 1)
+        imageView5.setImageResource(lottoImageStartId + result_sorted?.get(4) - 1)
+        imageView6.setImageResource(lottoImageStartId + result_sorted?.get(5) - 1)
     }
 }
