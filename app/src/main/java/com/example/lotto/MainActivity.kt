129e53fd0d0f5d49d84c1abd4707f8fe458735da
S-H JIN <jinsuhyeon00@naver.com>
2021-05-27 13:26:14 +0900

    Random number generation

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -5,22 +5,55 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.cardview.widget.CardView
+import java.util.*
+import kotlin.collections.ArrayList
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+fun getRandomLottoNumber () : Int{
+    return Random().nextInt(45)+1
+}
 
-        Toast.makeText(this, "Main Activity", Toast.LENGTH_LONG).show()
 
-        findViewById<View>(R.id.constellationCard).setOnClickListener {
-            startActivity(Intent(this,ConstellationActivity::class.java))
+fun getRandomLottoNumbers(): MutableList<Int>{
+    val lottoNumbers = mutableListOf<Int>()
+
+    while(true){
+        var numbers: Int = getRandomLottoNumber()
+        var flag_existing: Int = 0
+        if(lottoNumbers.contains(numbers)){
+            flag_existing =1
+            break;
         }
-        findViewById<View>(R.id.nameCard).setOnClickListener {
-            startActivity(Intent(this,NameActivity::class.java))
-        }
-        findViewById<View>(R.id.ResultCard).setOnClickListener {
-            startActivity(Intent(this,ResultActivity::class.java))
+        if(flag_existing.equals(1))
+            continue
+        else
+            lottoNumbers.add(numbers)
+        if(lottoNumbers.size>=0)
+            break;
+    }
+    return lottoNumbers
+}
+
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            Toast.makeText(this, "Main Activity", Toast.LENGTH_LONG).show()
+
+            val cardRandom = findViewById<CardView>(R.id.cardRandom)
+            cardRandom.setOnClickListener {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers ()))
+                startActivity(Intent(this, ResultActivity::class.java))
+
+            }
+            findViewById<View>(R.id.cardConstell).setOnClickListener {
+                startActivity(Intent(this, ConstellationActivity::class.java))
+            }
+            findViewById<View>(R.id.cardName).setOnClickListener {
+                startActivity(Intent(this, NameActivity::class.java))
+            }
+
         }
     }
-}
--- app/src/main/java/com/example/lotto/NameActivity.kt
+++ app/src/main/java/com/example/lotto/NameActivity.kt
@@ -1,7 +1,9 @@
 package com.example.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 
 class NameActivity : AppCompatActivity() {
@@ -10,5 +12,16 @@ class NameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_name)
 
         Toast.makeText(this,"Name Activity", Toast.LENGTH_LONG).show()
+
+        val btnGoResultName = findViewById<Button>(R.id.btnGoResultName)
+        val btnBack = findViewById<Button>(R.id.btnBack)
+
+        btnGoResultName.setOnClickListener {
+            startActivity(Intent(this,ResultActivity::class.java))
+        }
+
+        btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
