d6c2a2bd18afe09e37aa2ad9aa4ee44511f74a86
S-H JIN <jinsuhyeon00@naver.com>
2021-06-02 11:24:25 +0900

    Constellation-based hash code

--- app/src/main/java/com/example/lotto/ResultActivity.kt
+++ app/src/main/java/com/example/lotto/ResultActivity.kt
@@ -3,25 +3,33 @@ package com.example.lotto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ResultActivity : AppCompatActivity() {
     //    val imageView1id = R.id.imageView1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-        val result = intent.getIntegerArrayListExtra("result")// ?: return // ?: return
-        //        // 2) sort the array
-//        result.sort()
-//        val result_sorted = result?.sortedBy{it}
-        val result_sorted = result?.sorted()
-////        val result_sorted = result?.let { result.sortedBy{it} }
 
-//        result?.let{
-//            updateLottoBallImages(result.sortedBy{it})
-//        }
+        val result = intent.getIntegerArrayListExtra("result") ?: return
+        val sConstellation = intent.getStringExtra("constellation")
+
+        val result_sorted = result?.sorted()
+
+        sConstellation?.let{
+            val resultLabel = findViewById<TextView>(R.id.resultLabel)
+            resultLabel.text = "${sConstellation}의 ${SimpleDateFormat("yyyy년 MM월 dd일").format(Date())} 로또 번호입니다."
+        }
+       // result?.let{
+         //  updateLottoBallImages(result.sortedBy{it})
+        //}
+
         val lottoBallImageStartId = R.drawable.ball_01 // 146
-/*        val lottoBallImageId2 = R.drawable.ball_02 // 150
-        val lottoBallImageId3 = R.drawable.ball_03 // 148*/
+        val lottoBallImageId2 = R.drawable.ball_02 // 146
+        val lottoBallImageId3 = R.drawable.ball_01 // 146
+
         val imageView1 = findViewById<ImageView>(R.id.imageView1)
         val imageView2 = findViewById<ImageView>(R.id.imageView2)
         val imageView3 = findViewById<ImageView>(R.id.imageView3)
